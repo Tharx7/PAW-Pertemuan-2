69a38023a66149c99fbe2e418558a859b0c9b11c
Tharx7 <thoriqtajjala@gmail.com>
2022-08-30 18:47:46 +0700

    Output Pertemuan 2

---
+++ coba.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pert 2</title>
+</head>
+<body>
+
+  <h1>Output Pertemuan 2</h1>
+  <h3>Thariq Rifki Tajjala</h3>
+  <h4>Hasil :</h4>
+  <?php
+
+  function akuKesal ($nama, int $tinggibadan, float $beratbadan) {
+    echo "<p>Halo, Saya $nama Dengan Tinggi Badan $tinggibadan dan Berat Badan Saya $beratbadan";
+    }
+
+  akuKesal("Thariq", "165", "60");
+  akuKesal("Heru", "170", "55");
+  akuKesal("Yanuar", "166", "65");
+  ?>
+
+</body>
+</html>
